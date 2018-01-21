2613dfbd41693347a84c5e508e8034431b1d5fc2
Chris Dickson <christopherjdickson@gmail.com>
2018-01-21 15:35:47 -0600

    Add skeleton

---
+++ nextrip/direction.go
@@ -0,0 +1,8 @@
+package nextrip
+
+const (
+	DirectionSouth = 1
+	DirectionEast  = 2
+	DirectionWest  = 3
+	DirectionNorth = 4
+)
---
+++ nextrip/stop.go
@@ -0,0 +1,9 @@
+package nextrip
+
+type Stop struct {
+	ID          string
+	Description string
+}
+
+func GetStop(route string, direction int) {
+}
